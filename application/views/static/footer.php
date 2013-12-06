22189d296611488610096da4fe82058c79ecef3d
MarkyC <mmmarcooo@gmail.com>
2013-12-06 02:04:59 -0500

    Began footer redesign

--- application/views/static/footer.php
+++ application/views/static/footer.php
@@ -1,7 +1,7 @@
 <footer>
     <div class="container">
         <div class="row">
-            <div class="col-lg-4">
+            <div class="col-md-4">
                 <h1>Partners</h1>
                 <img src="http://placehold.it/150x60" alt="placeholder">
                 <img src="http://placehold.it/150x60" alt="placeholder">
@@ -14,9 +14,9 @@
                 <img src="http://placehold.it/150x60" alt="placeholder">
                 <img src="http://placehold.it/150x60" alt="placeholder">
             </div>
-            <div class="col-lg-8">
+            <div class="col-md-8">
                 <h1>Find Us</h1>
-                <div class="col-lg-6">
+                <div class="col-md-6">
                     <h2>Social Networks</h2>
                     <ul>
                         <li><i class="icon-twitter-sign icon-large"></i> Twitter: @FirstChoiceGarage</li>
@@ -26,7 +26,7 @@
                     <h2>Latest Tweets</h2>
                     <img src="http://placehold.it/290x250&text=tweets" alt="tweets">
                 </div>
-                <div class="col-lg-6">
+                <div class="col-md-6">
                     <h2>Location</h2>
                     <!-- Google Maps Location -->
                     <iframe width="290" height="300" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?f=q&amp;source=s_q&amp;hl=en&amp;q=First+Choice+Garage&amp;aq=&amp;sll=43.884037,-80.027161&amp;sspn=1.068974,2.705383&amp;ie=UTF8&amp;t=m&amp;st=100922986909904905661&amp;rq=1&amp;ev=zi&amp;split=1&amp;hq=First+Choice+Garage&amp;hnear=&amp;ll=43.794889,-79.546509&amp;spn=0.148691,0.198441&amp;z=11&amp;iwloc=A&amp;output=embed"></iframe><br /><small><a href="https://maps.google.com/maps?f=q&amp;source=embed&amp;hl=en&amp;q=First+Choice+Garage&amp;aq=&amp;sll=43.884037,-80.027161&amp;sspn=1.068974,2.705383&amp;ie=UTF8&amp;t=m&amp;st=100922986909904905661&amp;rq=1&amp;ev=zi&amp;split=1&amp;hq=First+Choice+Garage&amp;hnear=&amp;ll=43.794889,-79.546509&amp;spn=0.148691,0.198441&amp;z=11&amp;iwloc=A">View Larger Map</a></small>
