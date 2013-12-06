f6a81b7357a6a7ec0bf7ff678bc662872d8270f2
MarkyC <mmmarcooo@gmail.com>
2013-12-06 12:13:53 -0500

    Designed Geneva page, added to nav

--- application/views/cabinets/geneva/home.php
+++ application/views/cabinets/geneva/home.php
@@ -2,30 +2,30 @@
 
     <div class="row">
         <div class="col-md-4">
-            <img src="img/geneva/Locker-open.jpg" alt="Lockers">
+            <img src="img/cabinets/geneva/Locker-open.jpg" alt="Lockers">
             <h1>Lockers</h1>
         </div>
         <div class="col-md-4">
-            <img src="img/geneva/base-drawer.jpg" alt="Base Cabinets">
+            <img src="img/cabinets/geneva/base-drawer.jpg" alt="Base Cabinets">
             <h1>Base Cabinets</h1>
         </div>
         <div class="col-md-4">
-            <img src="img/geneva/cabinet-service.jpg" alt="Service Cabinets">
+            <img src="img/cabinets/geneva/cabinet-service.jpg" alt="Service Cabinets">
             <h1>Service Cabinets</h1>
         </div>
     </div>
 
     <div class="row">
         <div class="col-md-4">
-            <img src="img/geneva/wall-3.jpg" alt="Wall Storage">
+            <img src="img/cabinets/geneva/wall-3.jpg" alt="Wall Storage">
             <h1>Wall Storage</h1>
         </div>
         <div class="col-md-4">
-            <img src="img/geneva/shelf-3.jpg" alt="Wall-Units">
+            <img src="img/cabinets/geneva/shelf-3.jpg" alt="Wall-Units">
             <h1>Wall Units</h1>
         </div>
         <div class="col-md-4">
-            <img src="img/geneva/bench-6.jpg" alt="Benches">
+            <img src="img/cabinets/geneva/bench-6.jpg" alt="Benches">
             <h1>Benches</h1>
         </div>
     </div>
@@ -34,7 +34,7 @@
 
     <div class="row">
         <div class="col-xs-12">
-            <img src="img/geneva/package.jpg" alt="Package">
+            <img src="img/cabinets/geneva/package.jpg" alt="Package">
             <h1>Complete Geneva Package</h1>
             <h2>What you get:</h2>
             <ul>
