707f4d99a47d5517abbddb0dd30ac6061b756f6c
MarkyC <mmmarcooo@gmail.com>
2013-12-06 02:26:06 -0500

    broke dropdown menu

--- application/views/static/site_footer.php
+++ application/views/static/site_footer.php
@@ -1,9 +1,8 @@
-</body>
-
 <!-- JQuery, Bootstrap, LESS parser -->
 <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
-<script src="//netdna.bootstrapcdn.com/bootstrap/3.0.0-rc1/js/bootstrap.min.js"></script>
-<script src="//cdnjs.cloudflare.com/ajax/libs/less.js/1.4.1/less.min.js"></script>
+<script src="//netdna.bootstrapcdn.com/bootstrap/3.0.31/js/bootstrap.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/less.js/1.5.0/less.min.js"></script>
 <!-- Custom Site JS -->
 <script src="js/screen.js"></script>
+</body>
 </html>
