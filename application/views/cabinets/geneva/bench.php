2351c48f78798155d2f5ef125c729609317e0874
MarkyC <mmmarcooo@gmail.com>
2013-12-06 17:13:37 -0500

    Geneva Bench Page

---
+++ application/views/cabinets/geneva/bench.php
@@ -0,0 +1,35 @@
+<div class="container">
+
+    <h1>Geneva Benches</h1>
+
+    <div class="product">
+
+        <div class="row">
+            <div class="col-md-4">
+                <img class="img-responsive"
+                     src="<?php echo base_url(); ?>img/cabinets/geneva/bench-6.jpg"
+                     alt="6 foot Work Bench">
+            </div>
+            <div class="col-md-8">
+                <h1>6' Work Bench</h1>
+                <h2>6'L x 24"W 1.75"T</h2>
+                <p>Geneva work benches are designed to accommodate what ever you throw at them. Our 6 foot workbench features a solid hardwood maple top that is 1.75 inches thick and has 12 square feet of work surface available to handle most projects. Built on solid all steel welded frames and equipped with leg levellers that adjust to uneven floors.</p>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-md-4">
+                <img class="img-responsive"
+                     src="<?php echo base_url(); ?>img/cabinets/geneva/bench-8.jpg"
+                     alt="8 foot Work Bench">
+            </div>
+            <div class="col-md-8">
+                <h1>8' Work Bench</h1>
+                <h2>8'L x 24"W 1.75"T</h2>
+                <p>Geneva work benches are designed to accommodate what ever you throw at them. Our 8 foot workbench features a solid hardwood maple top that is 1.75 inches thick and has 12 square feet of work surface available to handle most projects. Built on solid all steel welded frames and equipped with leg levellers that adjust to uneven floors.</p>
+            </div>
+        </div>
+
+    </div> <!-- /product -->
+
+</div> <!-- /container -->
