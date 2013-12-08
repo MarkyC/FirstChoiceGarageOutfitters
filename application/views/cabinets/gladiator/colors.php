53352b81ed2190c084ee76a65b5ef0352cd9bfbd
MarkyC <mmmarcooo@gmail.com>
2013-12-08 17:25:05 -0500

    fixed image dimensions issues

--- application/views/cabinets/gladiator/colors.php
+++ application/views/cabinets/gladiator/colors.php
@@ -1,1 +1,1 @@
-Color: Hammered Granite <span style="display:inline-block; width:50px; height: 25px; background-color: black"></span>
+Color: Hammered Granite <span style="display:inline-block; width:50px; height: 20px; background-color: #1E1F17; border: 1px solid black"></span>
