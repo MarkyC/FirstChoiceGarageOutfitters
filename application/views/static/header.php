b8ed91b441993ed744596f860a8bb52fc2c89ce0
MarkyC <mmmarcooo@gmail.com>
2013-12-08 14:22:29 -0500

    Fixed header link

--- application/views/static/header.php
+++ application/views/static/header.php
@@ -1,6 +1,6 @@
 <!-- top logo -->
 <div class="logo">
     <div class="container">
-        <a href="index.html"><img src="<?php echo base_url(); ?>img/logo-header.png" alt="First Choice Garage Outfitters"></a>
+        <a href="<?php echo base_url(); ?>"><img src="<?php echo base_url(); ?>img/logo-header.png" alt="First Choice Garage Outfitters"></a>
     </div>
 </div>
