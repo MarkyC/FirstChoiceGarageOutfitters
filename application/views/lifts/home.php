6ac36885f4756bf0cde0925de659524cac6bf0c5
MarkyC <mmmarcooo@gmail.com>
2013-12-10 16:14:09 -0500

    Garage Height Calculator

--- application/views/lifts/home.php
+++ application/views/lifts/home.php
@@ -10,14 +10,7 @@
 
         <div class="row">
             <div class="col-md-4">
-                <img class="img-responsive" src="<?php echo base_url(); ?>img/lifts/propark-8s.jpg" alt="Pro Park Lifts">
-                <div class="alt-view">
-                    <a href="<?php echo base_url(); ?>img/lifts/propark-8s.jpg">
-                        <img class="img-responsive"
-                             src="<?php echo base_url(); ?>img/lifts/propark-8s.jpg"
-                             alt="Pro Park 8 Lifts">
-                    </a>
-                </div>
+                <img class="img-responsive" src="<?php echo base_url(); ?>img/lifts/propark-8s.jpg" alt="Pro Park 8 Lifts">
             </div>
             <div class="col-md-8">
                 <h1>Pro Park 8</h1>
@@ -35,7 +28,6 @@
                     <li>FREE Jack tray ($150 value)</li>
                     <li>FREE 3 Drip trays ($55 value)</li>
                     <li>FREE Caster kit ($345 value)</li>
-                    <li>Capacity 8,000 lbs.</li>
                     <li>Lifting Height 72"</li>
                     <li>Overall Length w/Approach Ramps 208"</li>
                     <li>Overall Width (with motor) 115-1/2"</li>
@@ -56,6 +48,189 @@
             </div>
         </div>
 
+        <div class="row">
+            <div class="col-md-4">
+                <img class="img-responsive" src="<?php echo base_url(); ?>img/lifts/PP8-plus-xl.jpg" alt="Pro Park 8 Plus Lifts">
+            </div>
+            <div class="col-md-8">
+                <h1>Pro Park 8 Plus</h1>
+                <p>The Pro Park 8 PLUS auto lift combines the advantages of the Pro Park 8S and 9+.Giving you 12” more in height.The Pro Park 8 PLUS provides the perfect solution for increasing parking space without having to resort to any permanent changes to your shop or property. This car hoist allows you to park any SUV or pick-up underneath, and is a versatile tool which can be used for any automotive lift and storage needs.</p>
+                <h2>Features</h2>
+                <ul>
+                    <li>Double your parking instantly</li>
+                    <li>Safety latches on all columns</li>
+                    <li>Single point lock release</li>
+                    <li>Low profile 37" approach ramps</li>
+                    <li>Powder coat finish</li>
+                    <li>Designed and tested to rigid industry standards</li>
+                    <li>8,000 lb. capacity</li>
+                    <li>Lock Ladder System</li>
+                    <li>Simple installation</li>
+                    <li>FREE Jack tray ($150 value)</li>
+                    <li>FREE 3 Drip trays ($55 value)</li>
+                    <li>FREE Caster kit ($345 value)</li>
+                    <li>Lifting Height 86" (7' 2")</li>
+                    <li>Overall Width 103 1/2" (8' 7 1/2")</li>
+                    <li>Overall Length 175 1/2" (14' 7 1/2")</li>
+                    <li>Approach Ramps 36 3/4" (3' 3/4")</li>
+                    <li>Runway Width 18 1/2" (1' 6 1/2")</li>
+                    <li>Runway Length 165 1/2" (13' 9 1/2")</li>
+                    <li>Runway Thickness 5"</li>
+                    <li>Width Between Runways 37 3/4"</li>
+                    <li>Width Between Runway Rails 32"</li>
+                    <li>Outside Runway to Outside Runway 74 3/4" (6' 2 3/4")</li>
+                    <li>Clearance Under Runways 81" (6' 9")</li>
+                    <li>Size of Column 6" x 3-1/4"</li>
+                    <li>Clearance Between Columns 94" (7' 10")</li>
+                    <li>Base of Column 10" x 12"</li>
+                    <li>Height of Columns 95 3/4" (7' 11 3/4")</li>
+                    <li>Drive Thru (Outside of tire to outside of tire) 80-1/2"</li>
+                    <li>Cable Diameter 3/8" Gauge Of Steel 6 Gauge (.1875"/ 3/16" / 4.75mm)</li>
+                    <li>Motor 110 Volt</li>
+                </ul>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-md-4">
+                <img class="img-responsive" src="<?php echo base_url(); ?>img/lifts/propark-9.jpg" alt="Pro Park 9 Plus Lifts">
+            </div>
+            <div class="col-md-8">
+                <h1>Pro Park 9 Plus</h1>
+                <p>This powerful, commercial grade, 4 post lift is the perfect tool to help increase the amount of parking space without having to resort to costly construction. You can double your parking space instantly, by using the Pro Park 9 Plus parking lift in your shop or home. Perfect for any car or full size SUV or pick-up</p>
+                <h2>Features</h2>
+                <ul>
+                    <li>Commercial grade</li>
+                    <li>Lock ladder leveling system</li>
+                    <li>Steel approach ramps</li>
+                    <li>Powder coat finish</li>
+                    <li>9,000 LB. capacity</li>
+                    <li>Lock Ladder System</li>
+                    <li>Double your parking instantly</li>
+                    <li>Single point lock release</li>
+                    <li>FREE Jack tray ($150 value)</li>
+                    <li>FREE 3 Drip trays ($55 value)</li>
+                    <li>FREE Caster kit w/ polyurethane wheels ($345 value)</li>
+                    <li>Motor 110V 1HP</li>
+                    <li>Shipping weight 2350 lbs.</li>
+                    <li>Speed of rise 75 seconds</li>
+                    <li>Lifting height in top lock position (Height to top of runway) 84”</li>
+                    <li>Clearance under lift in top lock position (Height to bottom of runway) 80”</li>
+                    <li>Length of runway 188”(15.8’)</li>
+                    <li>Length of approach ramp 31</li>
+                    <li>Height of columns (overall height) 97”</li>
+                    <li>Size of columns 7.75”x 4.75”</li>
+                    <li>Runway width 21-1/2”</li>
+                    <li>Width between runways 39”</li>
+                    <li>Measurement to outside both runways 79”</li>
+                    <li>Clearance between columns 101.5”</li>
+                    <li>Overall width (with motor) 121”</li>
+                    <li>Overall width (without motor) 113”</li>
+                </ul>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-md-4">
+                <img class="img-responsive" src="<?php echo base_url(); ?>img/lifts/jack-accessory-3500.jpg" alt="Pro Jack 3500">
+            </div>
+            <div class="col-md-8">
+                <h1>Pro Jack 3500</h1>
+                <p>The Pro Jack 3500 is an economical sliding jack that makes any four post lift complete. This versatile jack requires no air or electricity. It operates by the hydraulic hand pump, and is adjustable to fit most four-post lifts. Pro Jack 3500 comes standard with a set of peg style truck adaptors which increase the lifting height of the lift pads. These truck adapters will allow the Pro Jack 3500 sliding hydraulic jack to raise pick up trucks that will sit higher on the runways than the average passenger car. Pro Jack 3500 is operated by a manual hydraulic pump (porta-power pump) and does not require an air compressor for power. Pro Jack 3500 sliding hydraulic jack also features two solid locking positions that are used when the vehicle is to be suspended above the runways. When the locking positions of the Pro Jack 3500 are engaged, the hydraulic pressure from the lifting cylinder of the Pro Jack 3500 is eliminated. (These locks act much like a pair of jack stands after you lift a vehicle off the ground with a service jack).</p>
+                <h2>Features</h2>
+                <ul>
+                    <li>3,500 LB. capacity</li>
+                    <li>Manually operated</li>
+                    <li>Scissor mechanism allows for level lifting</li>
+                    <li>Compact size with a low profile</li>
+                    <li>Adjustable width fits most four post lifts</li>
+                    <li>Safety lock positions</li>
+                    <li>Adapters included</li>
+                    <li>Powder coated finish</li>
+                    <li>Smooth rolling action</li>
+                    <li>Adapter storage</li>
+                    <li>Capacity 3,500 LB.</li>
+                    <li>Maximum Width 41 1/2"</li>
+                    <li>Minimum Width 29 1/2"</li>
+                    <li>Minimum Width with Arms In 38 3/4"</li>
+                    <li>Maximum Width with Arms Out 61 1/2"</li>
+                    <li>Minimum Height with Lift Pad 3 1/2"</li>
+                    <li>Lifting Pad 1 3/8"</li>
+                    <li>Short Adapter 1 1/4"</li>
+                    <li>Tall Adapter 5"</li>
+                    <li>Raised Height without Adapter 11"</li>
+                    <li>Raised Height with Small Adapter 12 1/4"</li>
+                    <li>Raised Height with Large Adapter 16"</li>
+                    <li>Raised Height with Sm. & Lg. Adapter 17 1/4"</li>
+                </ul>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-md-4">
+                <img class="img-responsive" src="<?php echo base_url(); ?>img/lifts/loftharley.jpg" alt="Pro Deluxe Loft-It Storage Lift System">
+            </div>
+            <div class="col-md-8">
+                <h1>Deluxe Loft-It Storage Lift System</h1>
+                <h2>8"L x 4"W (Holds 1200lbs)</h2>
+                <p>The Deluxe Loft-It auto lift provides the perfect solution for increasing parking space without having to resort to any permanent changes to your shop or property. This motorcycle hoist allows you to double your parking space instantly, and is a versatile tool which can be used for any motorcycle lift and storage needs. </p>
+                <h2>Features</h2>
+                <ul>
+                    <li>Platform size of 4' x 8'"</li>
+                    <li>Lifting capacity of 1200 lbs</li>
+                    <li>Extremely safe and secure"</li>
+                    <li>Can't overload - The Loft-It system will not raise if the weight limit is exceeded""</li>
+                    <li>Electric Motor in the base of the unit"</li>
+                    <li>Screw Drive system locks lift and prevents sinking that can happen if hydraulics were used"</li>
+                    <li>Constructed from aircraft grade aluminum"</li>
+                    <li>Stainless Steel lifting cables"</li>
+                    <li>Locked with a key-controlled switch"</li>
+                    <li>Designed to last"</li>
+                    <li>One-year Limited Warranty"</li>
+                    <li>Stores flat against wall"</li>
+                    <li>Black anodized frame"</li>
+                    <li>Heavy duty PVC diamond plate flooring"</li>
+                    <li>Slat wall backing" </li>
+                </ul>
+            </div>
+        </div>
+
     </div> <!-- /product -->
 
 </div> <!-- /container -->
+
+
+<div id="calculator" class="container">
+    <h1>Garage Height Calculator</h1>
+    <div class="row">
+        <div class="col-lg-3 col-md-4 col-sm-12">
+            <img class="img-responsive" src="<?php echo base_url(); ?>img/lifts/pp8-ca27.jpg" alt="Calculate Height Pro Park Lift">
+        </div>
+        <div class="col-lg-3 col-md-4 col-sm-12">
+            <form role="form">
+                <div class="form-group">
+                    <label for="top"><strong>(b)</strong> Top vehicle height (inches)</label>
+                    <input type="number" class="form-control" id="top" name="top" placeholder="66">
+                </div>
+                <div class="form-group">
+                    <label for="bottom"><strong>(d)</strong> Drive under height (inches)</label>
+                    <select id="bottom" name="bottom" class="form-control">
+                        <option value="17">17"</option>
+                        <option value="23">23"</option>
+                        <option value="29">29"</option>
+                        <option value="35">35"</option>
+                        <option value="41">41"</option>
+                        <option value="47">47"</option>
+                        <option value="53">53"</option>
+                        <option value="59">59"</option>
+                        <option value="65">65"</option>
+                    </select>
+                </div>
+                <button type="button" onclick="calcHeight()" class="btn btn-default">Submit</button>
+            </form>
+        </div>
+        <div class="col-lg-6 col-md-4 col-sm-12">
+            <p class="result"></p>
+        </div>
+    </div> <!-- /row -->
+</div>
--- js/screen.js
+++ js/screen.js
@@ -10,3 +10,26 @@ $(".alt-view a").click(function(e) {
     e.preventDefault();
     $(this).parent().siblings(".img-responsive").attr('src', $(this).attr('href'));
 });
+
+/* Height Calculator */
+var calcHeight = function() {
+    var tHeight = $("#top").val();
+    var bHeight = $("#bottom").val();
+
+    if (tHeight == "") tHeight = 66;
+
+    if (isNaN(tHeight)) {
+        window.alert("Please enter a valid top height for (b)");
+    } else {
+        var total = parseInt(tHeight) + parseInt(bHeight) + 4 + 2;
+        var ftTotal = Math.floor(total / 12);
+        var inTotal = total % 12;
+
+        console.log("top: " + tHeight + "bottom: " + bHeight + "total: " + total);
+
+        // Set result
+        $('#calculator .result').html(
+            "You will need more than <strong>" + total + "</strong> inches (<strong>" + ftTotal + "' " + inTotal + '</strong>")'
+        );
+    }
+}
